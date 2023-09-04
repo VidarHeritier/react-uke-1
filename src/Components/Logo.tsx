12403d227358e6bcde87951085ea479592661c9c
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2023-09-04 11:59:01 +0200

    hopefully fixed pic issue

--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -1,8 +1,9 @@
+import LogoImage from "../Imgs/Artboard 3@0.5x copy.png";
 export function Logo() {
   return (
     <>
       <div>
-        <img className="logo" src="../public/Artboard 3@0.5x copy.png" alt="" />
+        <img className="logo" src={LogoImage} alt="" />
       </div>
     </>
   );
