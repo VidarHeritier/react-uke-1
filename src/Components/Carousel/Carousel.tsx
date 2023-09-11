558a03ad01c4454ff871b18bc0ccc0798808f169
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2023-09-11 13:51:58 +0200

    debugging 101.1b

--- src/Components/Carousel/Carousel.tsx
+++ src/Components/Carousel/Carousel.tsx
@@ -6,14 +6,14 @@ import { shoeInfo } from "../Shoes";
 
 import "./navigate-btns.css";
 
-interface ShoeInfo {
-  img?: string;
-  shoeName?: string;
-  brand?: string;
-  description?: string;
-  price?: string;
-  id?: string;
-}
+// interface ShoeInfo {
+//   img: string;
+//   shoeName: string;
+//   brand: string;
+//   description: string;
+//   price: string;
+//   id: string;
+// }
 
 function Carousel() {
   const [move, setMove] = useState(0);
@@ -29,7 +29,7 @@ function Carousel() {
           transition: "transform 0.3s ease-in-out",
         }}
       >
-        {shoeInfo.map((shoe: ShoeInfo) => {
+        {shoeInfo.map((shoe) => {
           return (
             <div key={shoe.id}>
               <ShoeCards
